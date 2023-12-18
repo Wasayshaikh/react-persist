9a8e34376732ada26cd2ed92cadac7d66145fdfc
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-18 21:20:41 +0500

    start working on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-
+import { Link, NavLink } from "react-router-dom"
 function App() {
 
   return (
     <>
-      <div className='flex justify-between items-center bg-[#f2f2f2] shadow-md shadow-gray-300 py-2'>
-        <div className='flex items-center'>
-          <p className='p-3 text-xl px-4'>Post List</p>
+      <div className='nav-container'>
+        <div className='nav-left'>
+          <p className='nav-item nav-heading'><Link className="nav-link" to={"#"}>Post List</Link></p>
 
           <ul className='flex item-center'>
 
-            <li className='p-3'>Posts</li>
-            <li className='p-3'>My posts</li>
+            <li className='nav-item'><Link  className="nav-link" to={"#"}>Posts</Link> </li>
+            <li className='nav-item'><Link  className="nav-link" to={"#"}>My posts</Link></li>
           </ul>
         </div>
-        <ul className='flex'>
-          <li className='p-3'>login</li>
-          <li className='p-3'>logout</li>
+        <ul className='nav-right'>
+          <li className='nav-item'><Link  className="nav-link" to={"#"}>login</Link></li>
+          <li className='nav-item'><Link  className="nav-link" to={"#"}>logout</Link></li>
         </ul>
       </div>
     </>
