b74048a72510abc9a56d8e63c41575188e0f59f7
abdul wasay <abdulwasayshaikh1999@gmail.com>
2023-12-25 22:28:26 +0500

    list of views

---
+++ src/views/index.ts
@@ -0,0 +1,5 @@
+export { default as Home} from "./Home";
+export { default as Post} from "./Post";
+export { default as Logout} from "./Logout";
+export { default as Login} from "./Login";
+export { default as ErrorPage} from "./ErrorPage";
