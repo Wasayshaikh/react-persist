d8e46076f8be0d8b111edd1414d234737a739d24
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-26 15:49:01 +0500

    get the state from redux

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,20 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { AddPost, getPosts } from '../redux/Slices/PostSlice';
+import { authUser, login, logout } from '../redux/Slices/AuthSlice';
+import { nanoid } from '@reduxjs/toolkit';
 
 const Home = () => {
-    type postType = {
-        posts: Array<{ id: number | string, title: string, body: string }>,
-    };
-    let posts: postType = {
-        posts: [{
-            id: 1, title: "Post One", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorum atque at ab. Tempora eveniet, vero distinctio sequi harum beatae saepe molestiae aut, recusandae odio officia amet nam vitae quia!"
-        },
-        {
-            id: 2, title: "Post two", body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque dolorum atque at ab. Tempora eveniet, vero distinctio sequi harum beatae saepe molestiae aut, recusandae odio officia amet nam vitae quia!"
-        }
-        ]
+    const posts = useSelector(getPosts);
+    const auth = useSelector(authUser);
+    const dispatch = useDispatch();
+    const handleClick = ()=> {
+        
+        dispatch(login());
+        dispatch(AddPost({id:nanoid(),title:"asdf", body:"ASdf"}))
+        
+
     }
-    const postComponent: React.ReactNode = posts.posts.map(post => (
+    const logouts = ()=> {
+        
+        dispatch(logout());
+        
+
+    }
+    
+    console.log(auth)
+    console.log(posts)
+    const postComponent: React.ReactNode = posts.map(post => (
         <div className="p-3" key={post.id}>
             <Link to={`/post/${post.id}`}>
             <div className="border-2 text-[#111111] border-[#f2f2f2] bg-[#f7f5f5] p-4 rounded-lg shadow-lg shadow-gray-300">
@@ -31,6 +42,12 @@ const Home = () => {
     ))
     return (
         <div className="container lg:max-w-[1200px] m-auto p-4">
+            <button onClick={()=>{
+                handleClick()
+            }}>Click </button>
+             <button onClick={()=>{
+                logouts()
+            }}>logout </button>
             {postComponent}
         </div>
     )
