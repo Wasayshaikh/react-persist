01065591a2546800020b5fc9fdc59aa0afc15514
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-26 15:47:39 +0500

    wrap the app in persistGate and provider component

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store, persistor } from './redux/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
   ,
 )
