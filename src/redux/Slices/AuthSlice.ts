a1adc768b624d9a1939d86aa32ea6709d2448748
Abdul wasay shaikh <abdulwasayshaikh1999@gmail.com>
2023-12-27 19:21:26 +0500

    add login functionality

--- src/redux/Slices/AuthSlice.ts
+++ src/redux/Slices/AuthSlice.ts
@@ -1,29 +1,64 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 type AuthType = {
-    user: Array<{ id: number | string, username: string, email: string }| []>,
+    user: { id: number | string, username: string, email: string }| null,
     status:boolean
 };
 let initialState: AuthType = {
-    user: [],
+    user: null,
     status:false
 }
+let users = [
+    {id:"1", email:"admin@email.com", username:"admin", password:"1234"},
+    {id:"2", email:"user@email.com", username:"user", password:"1234"},
+    {id:"3", email:"test@email.com", username:"test", password:"1234"},
+    {id:"4", email:"first@email.com", username:"first", password:"1234"},
+    {id:"5", email:"second@email.com", username:"second", password:"1234"}
+]
+export const loginAction = createAsyncThunk('Auth/loginAction',async (credentials:{email:string, password:string}, { rejectWithValue }) => {
+    try {
+        // Simulating login with hardcoded users array
+        const { email, password } = credentials;
+        const logged_in_user = users.find(
+          (user) => user.email === email && user.password === password
+        );
+  
+        if (logged_in_user) {
+          return logged_in_user; // Return the user data if login is successful
+        } else {
+          return rejectWithValue({ message: 'Invalid credentials' });
+        }
+      } catch (error) {
+        return rejectWithValue({ message: 'failed to connect' }); // Return the error response data
+      }
+})
 const AuthReducer = createSlice({
     name: "Auth",
     initialState,
     reducers: {
-        login(state) {
-            state.user.push({id:"Asdf",email:"asdf",username:"asdf"}),
-            state.status = true;
-        },
-        logout(state){
-            state.user =[],
+        logoutAction(state){
+            // const ca = users.find(user => user.email === 'admin@email.com' && user.password === '1234');
+            state.user =null,
             state.status = false;
         }
 
-    }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(loginAction.pending, (state) => {
+            state.status = false;
+        });
+        builder.addCase(loginAction.fulfilled, (state, action) => {
+          state.status = true;
+          state.user=action.payload; // Set the logged-in user data
+        });
+        builder.addCase(loginAction.rejected, (state,) => {
+          state.status = false;
+          state.user = null; // Set the error message
+        });
+      },
 
 })
-export const authUser = (state: { auth_user: AuthType }) => state.auth_user;
-export const { login, logout } = AuthReducer.actions
+export const authUser = (state: { auth_user: AuthType }) => state.auth_user.user;
+export const checkAuth = (state: { auth_user: AuthType }) => state.auth_user.status;
+export const { logoutAction } = AuthReducer.actions
+
 export default AuthReducer.reducer
